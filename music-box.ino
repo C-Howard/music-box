23606fc563d8bb6f6ef2167d9474cb4aa949f108
Cristyn Howard <cristyn.howard@mail.utoronto.ca>
2017-12-21 06:32:13 -0500

    Added Smoke on Water tune.

--- music-box.ino
+++ music-box.ino
@@ -13,14 +13,24 @@ const int soundPin = 13;
 
 int noteDur = 250; // ms per 1 beat
 
-long happyBirthday[50] = { 146.83, 1, 146.83, 1, 
-164.81, 2, 146.83, 2, 196.00, 2, 174.61, 4, 146.83, 1, 
-146.83, 1, 164.81, 2, 146.83, 2, 220.00, 2, 196.00, 4, 
-146.83, 1, 146.83, 1, 293.66, 2, 246.94, 2, 196.00, 
-2, 174.61, 2, 164.81, 2, 261.63, 1, 261.63, 1, 
-246.94, 2, 196.00, 2, 220.00, 2, 196.00, 4};
+int happyBirthdayLen = 50;
+long happyBirthday[50] = { 
+  146.83, 1, 146.83, 1, 164.81, 2, 146.83, 2, 196.00, 2, 
+  174.61, 4, 146.83, 1, 146.83, 1, 164.81, 2, 146.83, 2, 
+  220.00, 2, 196.00, 4, 146.83, 1, 146.83, 1, 293.66, 2, 
+  246.94, 2, 196.00, 2, 174.61, 2, 164.81, 2, 261.63, 1,
+  261.63, 1, 246.94, 2, 196.00, 2, 220.00, 2, 196.00, 4
+};
+
+int smokeOnWaterLen = 28;
+long smokeOnWater[28] = {
+  49.00, 2, 58.27, 2, 65.41, 2, 0, 1, 49.00, 1, 0, 1, 
+  58.27, 2, 69.30, 1, 65.41, 4, 49.00, 2, 58.27, 2,
+  65.41, 3, 58.27, 2, 49.00, 7};
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(switchAPin, INPUT);
   pinMode(switchBPin, INPUT);
   pinMode(switchCPin, INPUT);
@@ -33,10 +43,17 @@ void setup() {
 }
 
 void loop() {
+  
   if (digitalRead(switchAPin) == HIGH) {
     digitalWrite(ledAPin, HIGH);
-    playTune(happyBirthday, 50);
+    playTune(happyBirthday, happyBirthdayLen);
     digitalWrite(ledAPin, LOW);
+  }
+  
+  else if (digitalRead(switchBPin) == HIGH) {
+    digitalWrite(ledBPin, HIGH);
+    playTune(smokeOnWater, smokeOnWaterLen);
+    digitalWrite(ledBPin, LOW);
   }
 }
 
